81784d2d2ad4b7017f7c35b385aa777160eaec34
Jon Tamayo <volsungn@gmail.com>
2026-06-17 18:10:26 +0200

    Sort the vga re-export to satisfy rustfmt

--- crates/izarravm-video/src/lib.rs
+++ crates/izarravm-video/src/lib.rs
@@ -3,7 +3,7 @@ use thiserror::Error;
 
 pub mod margo;
 pub mod vga;
-pub use vga::{Vga, VGA_PLANAR_SIZE, VgaRaster};
+pub use vga::{VGA_PLANAR_SIZE, Vga, VgaRaster};
 
 pub use margo::{
     Channel, MARGO_ID_VALUE, MARGO_MMIO_SIZE, MARGO_VBE_MODES, MARGO_VRAM_SIZE, Margo,
