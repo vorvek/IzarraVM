1b98a19b6b4a4e328a2f6b336d64ba8bb3bb0eea
Jon Tamayo <volsungn@gmail.com>
2026-06-21 06:59:36 +0200

    toka-dos: stop tracking the packer build output
    pack/target was accidentally committed back in P0; it rebuilds on every pack run
    and does not belong in git. Untrack it and ignore it.

--- toka-dos/pack/target/debug/deps/toka_pack.d
+++
@@ -1,5 +0,0 @@
-D:\dev\IzarraVM\toka-dos\pack\target\debug\deps\toka_pack.d: src\main.rs
-
-D:\dev\IzarraVM\toka-dos\pack\target\debug\deps\toka_pack.exe: src\main.rs
-
-src\main.rs:
--- toka-dos/pack/target/debug/toka-pack.d
+++
@@ -1,1 +0,0 @@
-D:\dev\IzarraVM\toka-dos\pack\target\debug\toka-pack.exe: D:\dev\IzarraVM\toka-dos\pack\src\main.rs
